d4d681dcd128ed2f4efb524534235824cd550441
unknown <angusleung7@gmail.com>
2024-08-12 17:04:00 -0400

    change bootstrap import

--- frontend/kings-hockey/src/App.jsx
+++ frontend/kings-hockey/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <img src="/gav.jpg" alt="gav-png" width={400} height={600} />
       <img src="/dom.jpg" alt="gav-png" width={400} height={600} />
       <h1>Kings</h1>
+      <Button variant='primary'>test</Button>
     </div>
   );
 }
