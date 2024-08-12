84c97df08cd9b413a2140fd30f965306f547f8cd
dsincennes <Donaldsincennes@hotmail.com>
2024-08-12 16:42:14 -0400

    added header to page

---
+++ frontend/kings-hockey/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <img src="/gav.jpg" alt="gav-png" width={400} height={600} />
+      <img src="/dom.jpg" alt="gav-png" width={400} height={600} />
+      <h1>Kings</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/kings-hockey/src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Gavin & Dom are gay</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#features">Calendar</Nav.Link>
+            <Nav.Link href="#pricing">Gallery</Nav.Link>
+            <Nav.Link href="#about">Polls</Nav.Link>
+            <Nav.Link href="#about">Stats</Nav.Link>
+            <Nav.Link href="#about">Standings</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
