ae81fa31b3a1a418297f1f3a82e6c421908c1f1e
dsincennes <Donaldsincennes@hotmail.com>
2024-08-13 00:36:16 -0400

    reformatted calendar

--- frontend/kings-hockey/src/components/Calendar/index.jsx
+++ frontend/kings-hockey/src/components/Calendar/index.jsx
@@ -3,73 +3,196 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 
 const Calendar = () => {
-    const [openMonths, setOpenMonths] = useState({
-        'August 2024': true,
-    });
+  const [openMonths, setOpenMonths] = useState({
+    'August 2024': true,
+  });
 
-    const toggleMonth = (month) => {
-      setOpenMonths((prevOpenMonths) => ({
-        ...prevOpenMonths,
-        [month]: !prevOpenMonths[month],
-      }));
-    };
-    
-    const games = [
-        {
-          month: 'April 2024',
-          matches: [
-            { id: 1, home: 'Team Sexy', away: 'NE Bulldogs', result: 'Final', date: 'Mon, Apr 29', time: '9:15pm', location: 'Bell Sensplex' },
-          ],
-        },
-        {
-          month: 'May 2024',
-          matches: [
-            { id: 2, home: 'Shrew Crew', away: 'Team Sexy', result: 'Final', date: 'Mon, May 06', time: '10:00pm', location: 'Bell Sensplex' },
-            { id: 3, home: 'Team Sexy', away: 'Oakhart', result: 'Final', date: 'Mon, May 13', time: '10:15pm', location: 'Bell Sensplex' },
-            { id: 4, home: 'Team Sexy', away: 'Die Hards', result: 'Final', date: 'Tue, May 21', time: '8:15pm', location: 'Bell Sensplex' },
-            { id: 5, home: 'Team Sexy', away: '7 Irons', result: 'Final', date: 'Mon, May 27', time: '8:15pm', location: 'Bell Sensplex' },
-          ],
-        },
-        {
-          month: 'June 2024',
-          matches: [
-            { id: 6, home: 'Team Sexy', away: 'Shrew Crew', result: 'Final', date: 'Mon, Jun 03', time: '9:30pm', location: 'Bell Sensplex' },
-            { id: 7, home: 'Team Sexy', away: 'Warriors', result: 'Final', date: 'Sun, Jun 09', time: '8:15pm', location: 'Bell Sensplex' },
-            { id: 8, home: 'Team Sexy', away: 'Die Hards', result: 'Final', date: 'Mon, Jun 17', time: '9:15pm', location: 'Bell Sensplex' },
-            { id: 9, home: 'Team Sexy', away: 'Dangle dads', result: 'Final', date: 'Mon, Jun 24', time: '9:30pm', location: 'Bell Sensplex' },
-            { id: 10, home: 'Team Sexy', away: 'Puckheads', result: 'Final', date: 'Thu, Jun 27', time: '8:30pm', location: 'Bell Sensplex' },
-          ],
-        },
-        {
-          month: 'July 2024',
-          matches: [
-            { id: 11, home: 'Team Sexy', away: 'Warriors', result: 'Final', date: 'Sun, Jul 07', time: '9:45pm', location: 'Bell Sensplex' },
-            { id: 12, home: 'Team Sexy', away: 'Warriors', result: 'Final', date: 'Wed, Jul 10', time: '10:15pm', location: 'Bell Sensplex' },
-            { id: 13, home: 'Team Sexy', away: 'Bastone 2.0', result: 'Final', date: 'Tue, Jul 16', time: '8:15pm', location: 'Bell Sensplex' },
-            { id: 14, home: 'Team Sexy', away: 'Shrew Crew', result: 'Final', date: 'Sun, Jul 21', time: '8:15pm', location: 'Bell Sensplex' },
-            { id: 15, home: 'Team Sexy', away: 'Scorpions', result: 'Final', date: 'Thu, Jul 25', time: '7:15pm', location: 'Bell Sensplex' },
-            { id: 16, home: 'Team Sexy', away: 'Bastone 2.0', result: 'Final', date: 'Wed, Jul 31', time: '7:15pm', location: 'Bell Sensplex' },
-          ],
-        },
-        {
-          month: 'August 2024',
-          matches: [
-            { id: 17, home: 'Team Sexy', away: 'Dangle dads', result: 'Final', date: 'Tue, Aug 06', time: '10:00pm', location: 'Bell Sensplex' },
-            { id: 18, home: 'Team Sexy', away: 'Shrew Crew', result: 'Final', date: 'Thu, Aug 15', time: '10:15pm', location: 'Bell Sensplex' },
-          ],
-        },
-      ];
+  const toggleMonth = (month) => {
+    setOpenMonths((prevOpenMonths) => ({
+      ...prevOpenMonths,
+      [month]: !prevOpenMonths[month],
+    }));
+  };
 
-      
+  const games = [
+    {
+      month: 'April 2024',
+      matches: [
+        {
+          id: 1,
+          home: 'Team Sexy',
+          away: 'NE Bulldogs',
+          date: 'Mon, Apr 29',
+          time: '9:15pm',
+          location: 'Bell Sensplex',
+        },
+      ],
+    },
+    {
+      month: 'May 2024',
+      matches: [
+        {
+          id: 2,
+          home: 'Shrew Crew',
+          away: 'Team Sexy',
+          date: 'Mon, May 06',
+          time: '10:00pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 3,
+          home: 'Team Sexy',
+          away: 'Oakhart',
+          date: 'Mon, May 13',
+          time: '10:15pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 4,
+          home: 'Team Sexy',
+          away: 'Die Hards',
+          date: 'Tue, May 21',
+          time: '8:15pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 5,
+          home: 'Team Sexy',
+          away: '7 Irons',
+          date: 'Mon, May 27',
+          time: '8:15pm',
+          location: 'Bell Sensplex',
+        },
+      ],
+    },
+    {
+      month: 'June 2024',
+      matches: [
+        {
+          id: 6,
+          home: 'Team Sexy',
+          away: 'Shrew Crew',
+          date: 'Mon, Jun 03',
+          time: '9:30pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 7,
+          home: 'Team Sexy',
+          away: 'Warriors',
+          date: 'Sun, Jun 09',
+          time: '8:15pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 8,
+          home: 'Team Sexy',
+          away: 'Die Hards',
+          date: 'Mon, Jun 17',
+          time: '9:15pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 9,
+          home: 'Team Sexy',
+          away: 'Dangle dads',
+          date: 'Mon, Jun 24',
+          time: '9:30pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 10,
+          home: 'Team Sexy',
+          away: 'Puckheads',
+          date: 'Thu, Jun 27',
+          time: '8:30pm',
+          location: 'Bell Sensplex',
+        },
+      ],
+    },
+    {
+      month: 'July 2024',
+      matches: [
+        {
+          id: 11,
+          home: 'Team Sexy',
+          away: 'Warriors',
+          date: 'Sun, Jul 07',
+          time: '9:45pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 12,
+          home: 'Team Sexy',
+          away: 'Warriors',
+          date: 'Wed, Jul 10',
+          time: '10:15pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 13,
+          home: 'Team Sexy',
+          away: 'Bastone 2.0',
+          date: 'Tue, Jul 16',
+          time: '8:15pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 14,
+          home: 'Team Sexy',
+          away: 'Shrew Crew',
+          date: 'Sun, Jul 21',
+          time: '8:15pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 15,
+          home: 'Team Sexy',
+          away: 'Scorpions',
+          date: 'Thu, Jul 25',
+          time: '7:15pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 16,
+          home: 'Team Sexy',
+          away: 'Bastone 2.0',
+          date: 'Wed, Jul 31',
+          time: '7:15pm',
+          location: 'Bell Sensplex',
+        },
+      ],
+    },
+    {
+      month: 'August 2024',
+      matches: [
+        {
+          id: 17,
+          home: 'Team Sexy',
+          away: 'Dangle dads',
+          date: 'Tue, Aug 06',
+          time: '10:00pm',
+          location: 'Bell Sensplex',
+        },
+        {
+          id: 18,
+          home: 'Team Sexy',
+          away: 'Shrew Crew',
+          date: 'Thu, Aug 15',
+          time: '10:15pm',
+          location: 'Bell Sensplex',
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="calendar">
       {games.map((monthData, index) => (
         <div key={index} className="month">
           <h3 onClick={() => toggleMonth(monthData.month)} className="month-header">
             {monthData.month}
-            <span className="toggle-icon">
-              {openMonths[monthData.month] ? '-' : '+'}
-            </span>
+            <span className="toggle-icon">{openMonths[monthData.month] ? '-' : '+'}</span>
           </h3>
           {openMonths[monthData.month] && (
             <table>
@@ -77,7 +200,6 @@ const Calendar = () => {
                 <tr>
                   <th>HOME</th>
                   <th>AWAY</th>
-                  <th>W/L/T</th>
                   <th>DATE</th>
                   <th>TIME</th>
                   <th>LOCATION</th>
@@ -94,11 +216,6 @@ const Calendar = () => {
                     <td>
                       <Link to={`/game/${match.id}`} className="game-link">
                         {match.away}
-                      </Link>
-                    </td>
-                    <td>
-                      <Link to={`/game/${match.id}`} className="game-link">
-                        {match.result}
                       </Link>
                     </td>
                     <td>
--- frontend/kings-hockey/src/pages/Home/index.jsx
+++ frontend/kings-hockey/src/pages/Home/index.jsx
@@ -19,33 +19,7 @@ const Home = () => {
         <img src="/dom.jpg" alt="Dom" className="img-fluid" style={{ maxHeight: 600 }} />
       </div>
       <div className="col-12 mb-4">
-        <h2>Section 1</h2>
-        <p>
-          This is the first section of the page. It contains some introductory text about Kings
-          Hockey.
-        </p>
-        <button className="btn btn-primary">Primary Button</button>
-      </div>
-      <div className="col-12 mb-4">
-        <h2>Section 2</h2>
-        <p>
-          This is the second section. Here we could add more detailed information, images, or other
-          content.
-        </p>
-      </div>
-      <div className="col-12 mb-4">
-        <h2>Section 3</h2>
-        <p>
-          This is the third section. This section might talk about upcoming events, games, or other
-          relevant details.
-        </p>
-      </div>
-      <div className="col-12 mb-4">
-        <h2>Section 4</h2>
-        <p>
-          Finally, this section can include any closing remarks or additional links to social media,
-          contacts, etc.
-        </p>
+        <img src="/angus.jpg" alt="Dom" className="img-fluid" style={{ maxHeight: 600 }} />
       </div>
     </div>
   );
