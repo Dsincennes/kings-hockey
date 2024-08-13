602cee531c619d2f11f4f0f79ea9340481d053b6
dsincennes <Donaldsincennes@hotmail.com>
2024-08-12 20:21:36 -0400

    testing push pull

--- frontend/kings-hockey/src/App.jsx
+++ frontend/kings-hockey/src/App.jsx
@@ -5,9 +5,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <img src="/gav.jpg" alt="gav-png" width={400} height={600} />
-      <img src="/dom.jpg" alt="gav-png" width={400} height={600} />
+      <img src="/gav.jpg" alt="gav-png" width="auto" height={600} />
+      <img src="/dom.jpg" alt="gav-png" width="auto" height={600} />
       <h1>Kings</h1>
+      <button className="btn btn-primary">Primary Button</button>
+      <br></br>
+      <br></br>
+      <Button>Testing</Button>
       <Button variant='primary'>test</Button>
     </div>
   );
--- frontend/kings-hockey/src/components/Header/index.jsx
+++ frontend/kings-hockey/src/components/Header/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import './index.scss';
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg='primary' expand="lg">
       <Container>
         <Navbar.Brand href="/">Gavin is gay</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
+          <Nav className="ml-auto" >
             <Nav.Link href="#features">Calendar</Nav.Link>
             <Nav.Link href="#pricing">Gallery</Nav.Link>
             <Nav.Link href="#about">Polls</Nav.Link>
